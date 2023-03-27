9949cff81c0e71c486ad8ca980c758f7ea80ee7e
Vinicius Mendonça <viniciusmendonca@gec.inatel.br>
2023-03-27 19:23:11 -0300

    Deleção de usuário pelo id com Delete

--- src/main/java/com/viniciusmartins/workshopmongo/resources/UserResource.java
+++ src/main/java/com/viniciusmartins/workshopmongo/resources/UserResource.java
@@ -1,20 +1,18 @@
 package com.viniciusmartins.workshopmongo.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -54,5 +52,13 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
