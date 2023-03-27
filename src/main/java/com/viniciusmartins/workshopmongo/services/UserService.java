07acd6005a51927d5b29374967e19b3ede7445ba
Vinicius Mendonça <viniciusmendonca@gec.inatel.br>
2023-03-27 19:39:07 -0300

    Atualização de usuário pelo id com PUT

--- src/main/java/com/viniciusmartins/workshopmongo/services/UserService.java
+++ src/main/java/com/viniciusmartins/workshopmongo/services/UserService.java
@@ -35,15 +35,32 @@ public class UserService {
 	}
 	
 	public User insert(User obj) {
+		
 		return repository.insert(obj);
 	}
 	
 	public void delete(String id) {
+		
 		findById(id);
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		
+		User newObj = repository.findById(obj.getId()).get();
+		updateData(newObj, obj);
+		
+		return repository.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
+		
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
