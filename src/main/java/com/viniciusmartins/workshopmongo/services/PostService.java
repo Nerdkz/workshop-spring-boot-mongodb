422e7ff5200056145a153f3bd53f172cd2ec0d90
Vinicius Mendonça <viniciusmendonca@gec.inatel.br>
2023-03-28 01:46:02 -0300

    Obtendo um post por id

---
+++ src/main/java/com/viniciusmartins/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.viniciusmartins.workshopmongo.services;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.viniciusmartins.workshopmongo.repository.PostRepository;
+import com.viniciusmartins.workshopmongo.services.exception.ObjectNotFoundException;
+import domain.Post;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	
+	
+	public Optional<Post> findById(String id) {
+		
+		Optional<Post> post = repository.findById(id);
+		
+		if(!post.isPresent()) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		
+		return post;
+	}
+	
+}
