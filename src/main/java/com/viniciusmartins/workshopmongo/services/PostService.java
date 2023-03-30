873b53da25c7f0a46e4372b25731cc61d36155c6
Vinicius Mendonça <viniciusmendonca@gec.inatel.br>
2023-03-30 18:30:36 -0300

    Consulta simples com @Query

--- src/main/java/com/viniciusmartins/workshopmongo/services/PostService.java
+++ src/main/java/com/viniciusmartins/workshopmongo/services/PostService.java
@@ -30,7 +30,8 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text){
-		return repository.findByTitleContainingIgnoreCase(text);
+		//return repository.findByTitleContainingIgnoreCase(text);
+		return repository.searchTitle(text);	
 	}
 	
 }
